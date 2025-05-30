49a5db462252c8bab24b2acbd547635a42e31154
EddSalazarc <esalazarc@fcpn.edu.bo>
2025-05-30 17:57:26 -0400

    Mi commit sin archivos innecesarios

---
+++ frontend/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="app">
+    <Navegacion />
+    <router-view />
+  </div>
+</template>
+
+<script setup>
+import Navegacion from './componentes/Navegacion.vue';
+</script>
+
+<style>
+
+</style>
